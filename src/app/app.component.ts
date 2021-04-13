0d1d295f524ad7db9ba1f301dd01fa3ba9d4e1da
Mono <Mono@DESKTOP-BR5IUL7>
2021-04-12 20:29:17 -0500

    typed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+declare function funciona():void;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portafolioemmanueldl';
+
+  constructor(){
+
+  }
 }
---
+++ src/assets/js/funciones.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    var typed = new Typed(".typing", {
+        strings: ["FrontEnd Developer", "Designer UI/UX", "Freelancer", "Mexican"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        loop: true
+    });
+});
