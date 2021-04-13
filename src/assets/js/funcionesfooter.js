f3d3302187a73207f6364595174ab253e0f407a2
Mono <Mono@DESKTOP-BR5IUL7>
2021-04-13 17:23:40 -0500

    funcion typed en footer

---
+++ src/assets/js/funcionesfooter.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    var typed = new Typed(".typing-footer", {
+        strings: ["","Designers","Developers","Devs"],
+        typeSpeed: 200,
+        backSpeed: 50,
+        loop: true,
+    });
+});
