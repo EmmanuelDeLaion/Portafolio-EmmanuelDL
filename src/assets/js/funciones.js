02bd9277c44a9d45880b09f4b434ac7959f21e9b
Mono <Mono@DESKTOP-BR5IUL7>
2021-04-13 23:31:30 -0500

    imagenes proyectos

--- src/assets/js/funciones.js
+++ src/assets/js/funciones.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     var typed = new Typed(".typing", {
         strings: ["FrontEnd Developer", "Designer UI/UX", "Freelancer", "Mexican"],
         typeSpeed: 100,
-        backSpeed: 100,
+        backSpeed: 50,
         loop: true
     });
 });
