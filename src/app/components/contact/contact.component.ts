408b2667f2cad2cd52663a2422a7e26ce1e84ade
Mono <Mono@DESKTOP-BR5IUL7>
2021-05-31 17:26:34 -0500

    tab implementado con javascript

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
- import Swal from 'sweetalert2';
+import Swal from 'sweetalert2';
 import { MessageService } from '../../services/message.service';
 import { FormGroup, Validators, FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LoadingService } from '../../services/loading.service';
@@ -13,27 +13,31 @@ export class ContactComponent implements OnInit {
 
   formContacto: FormGroup;
   private validCorreo = /\S+@\S+\.\S+/;
-
+  myScritElementContact: HTMLScriptElement;
 
   constructor(
     private formBuilder: FormBuilder,
     private _MessageService: MessageService,
     private _loading: LoadingService
-  ) { 
+  ) {
     this.formContacto = this.formBuilder.group({
       nombre: ['', [Validators.required]],
       correo: ['', [Validators.email, Validators.pattern(this.validCorreo)]],
       numero: ['', [Validators.required]],
       mensaje: ['', [Validators.required]]
-    })
+    });
+
+    this.myScritElementContact = document.createElement("script");
+    this.myScritElementContact.src = "/assets/js/funcionesaboutme.js";
+    document.body.appendChild(this.myScritElementContact);
   }
 
   ngOnInit(): void {
   }
 
-  enviarMensaje(){
+  enviarMensaje() {
     this._loading.cargando = true;
-    if(this.formContacto.value.nombre != "" && this.formContacto.value.correo != "" && this.formContacto.value.numero != "" && this.formContacto.value.mensaje != ""){
+    if (this.formContacto.value.nombre != "" && this.formContacto.value.correo != "" && this.formContacto.value.numero != "" && this.formContacto.value.mensaje != "") {
       this._MessageService.sendMessage(this.formContacto.value).subscribe(
         res => {
           Swal.fire({
@@ -41,22 +45,22 @@ export class ContactComponent implements OnInit {
             title: 'Enviado',
             text: 'Su mensaje se envió con exito'
           }).then(
-            function(value){
+            function (value) {
               location.reload();
             }
           );
           this.formContacto.reset();
           this._loading.cargando = false;
         },
-        err =>{
+        err => {
 
         }
-      );         
-    }else{
+      );
+    } else {
       Swal.fire({
         icon: 'error',
         title: 'Verifique los campos',
-        text:'Favor de ingresar todos los campos correctamente'
+        text: 'Favor de ingresar todos los campos correctamente'
       });
       this._loading.cargando = false;
     }
