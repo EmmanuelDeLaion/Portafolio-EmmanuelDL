08b83464ce6e000ec11cae2941c72e2d77fa9028
Mono <Mono@DESKTOP-BR5IUL7>
2021-05-31 21:28:30 -0500

    se elimino seccion desktop en tab diseños

--- src/assets/js/tabs-designs.js
+++ src/assets/js/tabs-designs.js
@@ -1,45 +1,39 @@
 // tabs categoria diseños 
 var web = document.getElementById("web");
 var mobile = document.getElementById("mobile");
-var desktop = document.getElementById("desktop");
 var all = document.getElementById("all");
 
 var btnWeb = document.getElementById("btn-web");
 var btnMobile = document.getElementById("btn-mobile");
-var btnDesktop = document.getElementById("btn-desktop");
 var btnAll = document.getElementById("btn-all");
 
 
 // Galeria de imagenes
-var imagenes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+var imagenes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 var galeria = document.getElementById("galeria");
 
-var imagenesMobile = [1, 2, 3, 4,5];
+var imagenesMobile = [1, 2, 3, 4, 5];
 var galeriaMobile = document.getElementById("galeriamobile");
 
-var imagenesWeb = [1, 2, 3, 4, 5, 6];
+var imagenesWeb = [1, 2, 3, 4, 5, 6, 7];
 var galeriaWeb = document.getElementById("galeriaweb");
 
 function openWeb() {
     // display 
     web.style.display = "block";
     mobile.style.display = "none";
-    desktop.style.display = "none";
     all.style.display = "none";
     // transition 
     web.style.transitionDelay = "0.4s";
     mobile.style.transitionDelay = "0s";
-    desktop.style.transitionDelay = "0s";
     all.style.transitionDelay = "0";
     // color botones 
     btnWeb.style.background = "#ff0096";
     btnMobile.style.background = "transparent";
-    btnDesktop.style.background = "transparent";
     btnAll.style.background = "transparent";
     // background botones 
     btnWeb.style.color = "#ffff";
     btnMobile.style.color = "#020b16";
-    btnDesktop.style.color = "#020b16";
     btnAll.style.color = "#020b16";
 
 }
@@ -47,66 +41,54 @@ function openWeb() {
 function openMobile() {
     web.style.display = "none";
     mobile.style.display = "block";
-    desktop.style.display = "none";
     all.style.display = "none";
 
     web.style.transitionDelay = "0s";
     mobile.style.transitionDelay = "0.4s";
-    desktop.style.transitionDelay = "0s";
     all.style.transitionDelay = "0s";
 
     btnWeb.style.background = "transparent";
     btnMobile.style.background = "#ff0096";
-    btnDesktop.style.background = "transparent";
     btnAll.style.background = "transparent";
 
     btnWeb.style.color = "#020b16";
     btnMobile.style.color = "#ffff";
-    btnDesktop.style.color = "#020b16";
     btnAll.style.color = "#020b16";
 }
 
 function openDesktop() {
     web.style.display = "none";
     mobile.style.display = "none";
-    desktop.style.display = "block";
     all.style.display = "none";
 
     web.style.transitionDelay = "0s";
     mobile.style.transitionDelay = "0s";
-    desktop.style.transitionDelay = "0.4s";
     all.style.transitionDelay = "0s";
 
     btnWeb.style.background = "transparent";
     btnMobile.style.background = "transparent";
-    btnDesktop.style.background = "#ff0096";
     btnAll.style.background = "transparent";
 
     btnWeb.style.color = "#020b16";
     btnMobile.style.color = "#020b16";
-    btnDesktop.style.color = "#ffff";
     btnAll.style.color = "#020b16";
 }
 
 function openAll() {
     web.style.display = "none";
     mobile.style.display = "none";
-    desktop.style.display = "none";
     all.style.display = "block";
 
     web.style.transitionDelay = "0s";
     mobile.style.transitionDelay = "0s";
-    desktop.style.transitionDelay = "0s";
     all.style.transitionDelay = "0.4s";
 
     btnWeb.style.background = "transparent";
     btnMobile.style.background = "transparent";
-    btnDesktop.style.background = "transparent";
     btnAll.style.background = "#ff0096";
 
     btnWeb.style.color = "#020b16";
     btnMobile.style.color = "#020b16";
-    btnDesktop.style.color = "#020b16";
     btnAll.style.color = "#ffff";
 }
 
